526cf4a3e7aba1b6516e27731b8d7865db5ec3f5
SHAIK MAIRAJ <skmairaj04@gmail.com>
2026-09-19 15:36:00 +0530

    Improve homepage campaign banner visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <Hero />
-      <HomeHub />
       <CampaignBanner />
+      <HomeHub />
       <FinalCta />
       <Footer />
     </main>
